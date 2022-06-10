ea41086bd6397f7dc921f2d9b8a0dea310aac708
TheCez <achodankar28@gmail.com>
2022-06-11 00:45:23 +0530

    Fix ping

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,5 +1,5 @@
 import shutil, psutil
-import time
+from time import time
 from signal import signal, SIGINT
 from os import path as ospath, remove as osremove, execl as osexecl
 from subprocess import run as srun, check_output
